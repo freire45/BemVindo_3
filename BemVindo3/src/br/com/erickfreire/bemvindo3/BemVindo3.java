c9b901ab67d61146502cfffe8c28e2cbe4333b82
Erick Freire <erilefr@gmail.com>
2022-10-02 21:31:36 -0300

    versao1
    versao1

---
+++ BemVindo3/src/br/com/erickfreire/bemvindo3/BemVindo3.java
@@ -0,0 +1,16 @@
+package br.com.erickfreire.bemvindo3;
+
+/**
+ * Bem Vindo a Programação em Java - Versão 3
+ * @author Erick Freire
+ * @version 1 - Criado em 02-10-2022
+ */
+
+public class BemVindo3 {
+
+	public static void main(String[] args) {
+		System.out.println("Bem Vindo\nA\nProgramação\nEm Java!");
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BemVindo_3
  Bem Vindo a Programação em Java - Versão 3
+
+## Image
+![Currículo no Computador](bem_vindo3.png)
+
